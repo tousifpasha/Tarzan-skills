a5785c4a08a4792355ec67168aaa93d7c9b29b20
tousifpasha <tousifshoaib@gmail.com>
2019-10-16 17:15:04 +0530

    basic python programs

---
+++ exercise.py
@@ -0,0 +1,7 @@
+# Area of a circle Program
+pi = 3.14
+r = 3
+result=pi*r**2
+print("pi=3.14 and radius=3")
+
+print("Area of a circle = ",result)
---
+++ strings.py
@@ -0,0 +1,20 @@
+name='tousif'
+print(name*2)
+spam="python"
+print(spam[2])
+print(spam[-1])
+print(spam[1:5])
+a = 'b'
+c = 5
+print(a + str(c))
+
+str1="This is python programming"
+print(str1.split(" "))
+print(str1.split("p"))
+
+str2="    python   is "
+print(str2.strip())
+print(str2.rstrip())
+
+str3=['s','a','l','m','a','n']
+print(" - ".join(str3))
