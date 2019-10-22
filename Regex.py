f356aef85e799ae5366de6f5bd2f6cdf3b7ddf8e
tousifpasha <tousifshoaib@gmail.com>
2019-10-22 16:29:16 +0530

    regular expression

---
+++ Regex.py
@@ -0,0 +1,25 @@
+import re
+# PhoneNumRegex=re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)')
+# mo= PhoneNumRegex.search('My Number is 415-555-4242')
+# print('Phone number found:' + mo.group())
+# print(mo.group())
+# print(mo.group(0))
+# print(mo.group(1))
+# print(mo.group(2))
+# print(mo.group(3))
+
+# batregex = re.compile(r'Bat(wo)?man')
+# mo1= batregex.search('The Adventures of Batman')
+# print(mo1.group())
+# mo2= batregex.search('The Adventures of Batwoman')
+# print(mo2.group())
+
+# match_num = re.compile(r'(\d{1,3})(,\d{3})*')
+# m1 = match_num.search('first number is 42')
+# m2 = match_num.search('second number is 1,234')
+# m3 = match_num.search('Third number is 6,368,745')
+# print(m1.group())
+# print(m2.group())
+# print(m3.group())
+
+
