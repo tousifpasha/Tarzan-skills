bc513abe7418fdf57787c8045210f6b63e343a23
tousifpasha <tousifshoaib@gmail.com>
2019-10-23 16:30:39 +0530

    iterators and generators and object oriented programs

---
+++ iterators.py
@@ -0,0 +1,8 @@
+evens= [2,4,6,8,10]
+eveniterator=iter(evens)
+print(eveniterator)
+for i in evens:
+    print(next(eveniterator))
+#print(next(eveniterator))
+
+
---
+++ listcomprehension.py
@@ -0,0 +1,10 @@
+list=[0,1,2,3,4]
+# x=[i**2 for i in list]
+# print(x)
+
+
+a=[x for x in list if x%2==0]
+print(a)
+
+# z=[x for x in range(11) if x%2==0]
+# print(z)
