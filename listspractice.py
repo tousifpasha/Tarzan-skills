a9c66be6e2d4e979599042f6a38759f9b0649c8f
tousifpasha <tousifshoaib@gmail.com>
2019-11-08 16:25:06 +0530

    practice

---
+++ listspractice.py
@@ -0,0 +1,9 @@
+prices = []
+temps = [32.0, 212.0, 0.0, 81.6]
+words = ['hello', 'world']
+car_details = ['Toyota', 'RAV4', 2.2, 60807]
+everything = [prices, temps, words, car_details]
+
+print(everything[1])
+print(everything[1][1])
+
